cab68c33c860e7d9ce745afa77ef8fa649772b73
cjrdz <jonathanrdzdev@gmail.com>
2025-03-25 11:10:53 -0600

    Second week corrections

--- 8-goOperators/operators.go
+++ 8-goOperators/operators.go
@@ -24,13 +24,112 @@ func addition() {
 	)
 
 	// Printing the sum
+	fmt.Println("Arithmetic Operators")
+	fmt.Println("***************************************")
+	fmt.Println("Addition method")
+	fmt.Println("Sum of", + a, "and", b, "=", sum)
 	fmt.Println("Sum: ", sum)
 	fmt.Println("Sum All: ", sumAll)
 	fmt.Println("Sum Total: ", sumTotal)
+}
+
+// Declaring subtraction function
+func subtraction() {
+	// Declaring variables
+	var a int = 20
+	var b int = 10
+	var subtraction int = a - b
+
+	// Printing the difference
+	fmt.Println("***************************************")
+	fmt.Println("Subtraction method")
+	fmt.Println("Difference between", + a, "and", b, "=", subtraction)
+}
+
+// Declaring multiplication function
+func multiplication() {
+	// Declaring variables
+	var a int = 10
+	var b int = 20
+	var multiplication int = a * b
+
+	// Printing the product
+	fmt.Println("***************************************")
+	fmt.Println("Multiplication method")
+	fmt.Println("Product of", + a, "by", b, "=", multiplication)
+}
+
+// Declaring division function
+func division() {
+	// Declaring variables
+	var a int = 20
+	var b int = 10
+	var division int = a / b
+
+	// Printing the quotient
+	fmt.Println("***************************************")
+	fmt.Println("Division method")
+	fmt.Println("Quotient of", + a, "and", b, "=", division)
+}
+
+// Declaring modulus function
+func modulus() {
+	// Declaring variables
+	var a int = 20
+	var b int = 10
+	var modulus int = a % b
+
+	// Printing the remainder
+	fmt.Println("***************************************")
+	fmt.Println("Modulus method")
+	fmt.Println("Remainder of", + a, "and", b, "=", modulus)
+}
+
+// Declaring increment function
+func increment() {
+	// Declaring variables
+	var a int = 10
+	var increment int = a
+
+	// Incrementing the variable
+	increment++
+
+	// Printing the increment
+	fmt.Println("***************************************")
+	fmt.Println("Increment method")
+	fmt.Println("Increment of", + a, "=", increment)
+}
+
+// Declaring decrement function
+func decrement() {
+	// Declaring variables
+	var a int = 10
+	var decrement int = a
+
+	// Decrementing the variable
+	decrement--
+
+	// Printing the decrement
+	fmt.Println("***************************************")
+	fmt.Println("Decrement method")
+	fmt.Println("Decrement of", + a, "=", decrement)
+	fmt.Println("***************************************")
 }
 
 // Declaring main function
 func main() {
 	// Calling addition function
 	addition()
+	// Calling subtraction function
+	subtraction()
+	// Calling multiplication function
+	multiplication()
+	// Calling division function
+	division()
+	// Calling modulus function
+	modulus()
+	// Calling increment function
+	increment()
+	// Calling decrement function
+	decrement()
 }
